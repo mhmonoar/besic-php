b690867f32383bab3b6be7a811d35269ce7a588e
mhmonoar <pra.wordpress@gmail.com>
2025-01-04 20:01:01 +0600

    create if else and switch file

--- 1if-else.php
+++ 1if-else.php
@@ -1,8 +1,35 @@
 <?php
-	$i=10;
-	if($i<=10){
-		echo $i++;
+	$i=20;
+	//$i = 17; (for elseif)
+	//$i = 14; (for else)
+	if($i>=18){
+		echo "Your age is: {$i} You're eligible for vot";
+	}elseif($i>=15){
+		echo "Your age is: {$i} You're not elegible for vot";;
 	}else{
-		echo $i."not run";
+		echo "Your condition is not valid";
+	}
+	
+	
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+	
+	//2nd example for if,elseif,else
+	$a =75;
+	
+	if($a<=100 && $a>=80){
+		echo "You are in marit list got A+";
+	}elseif($a<80 && $a >=70){
+		echo "You are not in marit list but you got A";
+	}elseif($a<70 && $a >= 60){
+		echo "You got B+";
+	}elseif($a<60 && $a>=33){
+		echo "You got C";
+	}elseif($a<33){
+		echo "You got 'F' and you are Fail";
+	}else{
+		echo "Your input number is Wrong";
 	}
 ?>
---
+++ 2switch.php
@@ -0,0 +1,28 @@
+<?php
+	$weekday = 7;
+	switch($weekday){
+		case 1:
+			echo "Today is Saturday";
+		break;
+		case 2:
+			echo "Today is Sunday";
+		break;
+		case 3:
+			echo "Today is Monday";
+		break;
+		case 4:
+			echo "Today is Tuesday";
+		break;
+		case 5:
+			echo "Today is Wesnesday";
+		break;
+		case 6:
+			echo "Today is Thusday";
+		break;
+		case 7:
+			echo "Today is Friday";
+		break;
+		default:
+			echo "Wrond input";
+	}
+?>
